cb704f4c9371231c6959b907b095cbcf6c9610a2
Mayank Kumar <mayankkumar@Mayanks-MacBook-Pro.local>
2023-05-06 11:16:08 +0530

    aas

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIARBD62VJ2XISBV4H4"
-  secret_key = "SmVwFNt1QFoPM/SDjSy74r+aEHcaHRmXF9C5j6RK"
+  region     = "us-west-2"
+  access_key = "YOUR_ACCESS_KEY"
+  secret_key = "YOUR_SECRET_KEY"
 }
